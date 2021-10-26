d261d496d314d3c35c16df8ea170e9e498bfb3c9
fmekota <filip@mekota.eu>
2021-10-26 15:20:03 +0200

    DB connect

--- jobboard/backend/core/config.py
+++ jobboard/backend/core/config.py
@@ -1,5 +1,20 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_TITLE:  str = "Jobboard"
     PROJECT_VERSION:    str = "0.1.1"
+
+    POSTGRES_USER:  str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD:   str = "aab44ke3Yd4bONgE"
+    POSTGRES_SERVER:    str = "34.91.227.127"
+    POSTGRES_PORT:  str = "5432"
+    POSTGRES_DB:    str = "fmekota"
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
 
 settings = Settings()
