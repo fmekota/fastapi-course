8d3b5efc20ffb1b0ebd98ec0d0db4b2f9e5819f9
fmekota <filip@mekota.eu>
2021-10-27 10:59:51 +0200

    Cleanup - __init__

--- jobboard/backend/main.py
+++ jobboard/backend/main.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
-from db.base import Base
+from db.base_class import Base
+from apis.version1.route_users import router
 
 
 def create_tables():
+
     Base.metadata.create_all(bind=engine)
 
 
 def start_application():
-    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+    app1 = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     create_tables()
-    return app
+    app1.include_router(router)
+    return app1
+
 
 app = start_application()
 
+
 @app.get("/")
 def hello_api():
-    return {"detail":"Hello World!"}
-
+    return {"detail": "Hello World!"}
