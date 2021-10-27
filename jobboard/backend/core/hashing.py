049a15d2c8e403b31e902b829bb43618d92f391a
fmekota <filip@mekota.eu>
2021-10-27 10:08:18 +0200

    DB connect

--- jobboard/backend/core/hashing.py
+++ jobboard/backend/core/hashing.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext
 
-pwt_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
+pwt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Hasher():
 
