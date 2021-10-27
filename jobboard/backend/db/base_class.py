d9a40041c5cc2bab16e7adf50f91aee5db941580
fmekota <filip@mekota.eu>
2021-10-27 10:02:06 +0200

    DB connect

--- jobboard/backend/db/base_class.py
+++ jobboard/backend/db/base_class.py
@@ -7,5 +7,8 @@ class Base:
     id: Any
     __name__: str
 
+    @declared_attr
     def __tablename__(cls)->str:
         return cls.__name__.lower()
+
+    __table_args__ = {'extend_existing': True}
